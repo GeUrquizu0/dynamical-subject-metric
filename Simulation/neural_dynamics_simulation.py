f6a1e80aa3d27ebe9dbefc8861634868e2c81952
J.G. Urquizu O. <gerardourquizugrajeda@gmail.com>
2026-03-27 18:32:57 -0700

    neural_dynamics_simulation.py
    neural_dynamics_simulation

--- Simulation/neural_dynamics_simulation.py
+++ Simulation/neural_dynamics_simulation.py
@@ -1,1 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+# number of neurons
+N = 20
+
+# random connectivity
+W = np.random.randn(N, N) * 0.3
+
+# simulation parameters
+dt = 0.01
+T = 1000
+
+x = np.random.randn(N)
+
+trajectory = []
+
+for t in range(T):
+
+    noise = 0.05 * np.random.randn(N)
+
+    dx = -x + np.tanh(W @ x) + noise
+
+    x = x + dt * dx
+
+    trajectory.append(x.copy())
+
+trajectory = np.array(trajectory)
+
+plt.plot(trajectory[:,0])
+plt.title("Example neural trajectory")
+plt.show()
